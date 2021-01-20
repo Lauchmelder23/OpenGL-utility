d9cd0f19cb2001ecbd9fc8cc2675898e3e735806
Robert <robert.trololo@gmail.com>
2021-01-20 17:58:55 +0100

    made shader transparent

--- examples/debug/main.cpp
+++ examples/debug/main.cpp
@@ -74,7 +74,7 @@ int main(int argc, char** argv)
 
 		oglu::ClearScreen(GL_COLOR_BUFFER_BIT, oglu::Color(0.29f, 0.13f, 0.23f));
 
-		shader->SetUniform("ourColor", &oglu::Color::Blue);
+		shader->SetUniform("ourColor", &oglu::Color::Transparent);
 		shader->Use();
 		square.BindAndDraw();
 
